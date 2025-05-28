736c2e486dc69650a5afa76a864f0a310c2e3e5a
Carlos Navarrete <imnaava@gmail.com>
2025-05-28 00:23:11 -0400

    Se crea archivo category.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,12 +5,14 @@ import { UserModule } from './modules/user/user.module';
 import { ConfigModule } from '@nestjs/config';
 import { ProductController } from './modules/product/controller/product/product.controller';
 import { ProductService } from './modules/product/service/product/product.service';
+import { CategoryModule } from './modules/category/category.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({isGlobal: true}),
     TypeOrmModule.forRootAsync(typeOrmConfig),
     UserModule,
+    CategoryModule,
   ],
   controllers: [ProductController],
   providers: [ProductService],
